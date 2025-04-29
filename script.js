a2242dd5b76f1235abb165de8c6581b950e5f318
Xualanm7142 <xlam7142@gmail.com>
2025-04-29 22:38:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
 const upload = document.getElementById("upload");
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-let originalImage = null;
+const img = document.getElementById("img");
+
+const brightness = document.getElementById("brightness");
+const contrast = document.getElementById("contrast");
+const saturate = document.getElementById("saturate");
 
 upload.addEventListener("change", (e) => {
   const file = e.target.files[0];
   const reader = new FileReader();
 
-  reader.onload = function (event) {
-    const img = new Image();
-    img.onload = function () {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx.drawImage(img, 0, 0);
-      originalImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    };
-    img.src = event.target.result;
+  reader.onload = () => {
+    img.src = reader.result;
   };
 
   reader.readAsDataURL(file);
 });
 
-function applyGrayscale() {
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  const data = imageData.data;
-
-  for (let i = 0; i < data.length; i += 4) {
-    const avg = (data[i] + data[i+1] + data[i+2]) / 3;
-    data[i] = data[i+1] = data[i+2] = avg;
-  }
-
-  ctx.putImageData(imageData, 0, 0);
+function updateFilter() {
+  img.style.filter = `
+    brightness(${brightness.value}%)
+    contrast(${contrast.value}%)
+    saturate(${saturate.value}%)
+  `;
 }
 
-function resetImage() {
-  if (originalImage) {
-    ctx.putImageData(originalImage, 0, 0);
-  }
-}
+[brightness, contrast, saturate].forEach(input => {
+  input.addEventListener("input", updateFilter);
+});
